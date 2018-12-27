80e3ed4cc066fb2a70512e6fd044e3e5a4b7d675
Radmir Kim <43144633+RealGoth@users.noreply.github.com>
2018-12-27 21:29:01 +0300

    Add files via upload

---
+++ Lab2/Lab2/Lab2/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Lab2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Rectangle rect = new Rectangle(5, 4);
+            Square square = new Square(5);
+            Circle circle = new Circle(5);
+            
+            rect.Print();
+            square.Print();
+            circle.Print();
+        }
+    }
+}
